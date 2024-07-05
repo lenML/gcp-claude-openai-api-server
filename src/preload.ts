33f9e8bbf3cfdd55b757a76f6898610bc476a6b3
zhzluke96 <zhzluke96@outlook.com>
2024-07-05 19:35:47 +0800

    fix: nexe deps bug

--- src/preload.ts
+++ src/preload.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import { Request, Response, Headers } from "node-fetch";
 import fetch from "node-fetch";
+import { AbortController, AbortSignal } from "node-abort-controller";
 
 dotenv.config();
 
@@ -10,4 +11,9 @@ if (!globalThis.fetch) {
   globalThis.Request = Request as any;
   globalThis.Response = Response as any;
   globalThis.Headers = Headers as any;
+}
+// NOTE: nexe 最高只有 node14 ...
+if (!globalThis.AbortController) {
+  globalThis.AbortController = AbortController as any;
+  globalThis.AbortSignal = AbortSignal as any;
 }
