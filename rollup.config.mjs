1782178274fa13408b5a4d1e0dcc1a48720baaed
zhzluke96 <zhzluke96@outlook.com>
2024-07-05 17:29:53 +0800

    feat add builder
    - 增加 打包

---
+++ rollup.config.mjs
@@ -0,0 +1,70 @@
+import typescript from "rollup-plugin-typescript2";
+import babel from "rollup-plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
+import esbuild from "rollup-plugin-esbuild";
+import MagicString from "magic-string";
+
+// NOTE: 不知道为什么... nexe 打包的环境没法识别 node:xxx 的模块... 所以这里简单替换一下
+function replaceNodePrefix() {
+  return {
+    name: "replace-node-prefix",
+    renderChunk(code) {
+      const magicString = new MagicString(code);
+      let hasReplaced = false;
+
+      const regex = /require\('node:(\w+)'\)/g;
+      let match;
+
+      while ((match = regex.exec(code)) !== null) {
+        magicString.overwrite(
+          match.index,
+          match.index + match[0].length,
+          `require('${match[1]}')`
+        );
+        hasReplaced = true;
+      }
+
+      if (!hasReplaced) {
+        return null;
+      }
+
+      return {
+        code: magicString.toString(),
+        map: magicString.generateMap({ hires: true }),
+      };
+    },
+  };
+}
+
+export default {
+  input: "src/server.ts",
+  output: {
+    dir: "output",
+    format: "cjs",
+  },
+  plugins: [
+    json(),
+    commonjs({
+      include: /node_modules/,
+    }),
+    esbuild({
+      include: /\.[jt]sx?$/,
+      // exclude: /node_modules/,
+      sourceMap: false,
+      minify: process.env.NODE_ENV === "production",
+      target: "node14",
+      define: {
+        "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`,
+      },
+      tsconfig: "tsconfig.json",
+      loaders: {
+        ".json": "json",
+        ".js": "jsx",
+      },
+    }),
+    resolve(),
+    replaceNodePrefix(),
+  ],
+};
--- src/preload.ts
+++ src/preload.ts
@@ -1,3 +1,13 @@
 import dotenv from "dotenv";
+import { Request, Response, Headers } from "node-fetch";
+import fetch from "node-fetch";
 
 dotenv.config();
+
+// NOTE: 为了兼容 nexe 环境
+if (!globalThis.fetch) {
+  globalThis.fetch = fetch as any;
+  globalThis.Request = Request as any;
+  globalThis.Response = Response as any;
+  globalThis.Headers = Headers as any;
+}
